8b44e7ffff8bd4498425c4b5f38330e819d9e840
James Butcher <jamesbutcher167@gmail.com>
2022-01-16 16:36:12 +0000

    Add system_name module

--- src/modules.rs
+++ src/modules.rs
@@ -1,11 +1,15 @@
 extern crate chrono;
 
-use sysinfo::{System, SystemExt};
 use chrono::prelude::*;
+use sysinfo::{System, SystemExt};
 
-pub fn uptime(prefix: String, sys: System) -> String {
+pub fn uptime(prefix: String, sys: &System) -> String {
     let naive = NaiveDateTime::from_timestamp(sys.uptime().try_into().unwrap(), 0);
     let datetime: DateTime<Utc> = DateTime::from_utc(naive, Utc);
 
     format!("{} {}", prefix, datetime.format("%H:%M:%S"))
+}
+
+pub fn system_name(prefix: String, sys: &System) -> String {
+    format!("{} {}", prefix, sys.name().unwrap())
 }
