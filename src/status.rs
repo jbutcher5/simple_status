c66ce655057216ef6a2ae1b0b9f906878a07daa6
James Butcher <jamesbutcher167@gmail.com>
2022-01-16 21:54:40 +0000

    Format code

--- src/status.rs
+++ src/status.rs
@@ -1,25 +1,29 @@
-use x11::xlib::*;
 use std::ffi::CString;
 use std::ptr;
+use x11::xlib::*;
 
 pub struct Status {
-    data: String,
-    display: *mut Display
+    pub data: String,
+    display: *mut Display,
 }
 
 impl Status {
     pub fn new(data: String) -> Self {
         Self {
             data,
-            display: unsafe{ XOpenDisplay(ptr::null()) }
+            display: unsafe { XOpenDisplay(ptr::null()) },
         }
     }
 
-    pub fn set_status(&self){
+    pub fn set_status(&self) {
         unsafe {
             let c_str = CString::new(self.data.as_str()).unwrap();
-            XStoreName(self.display, XDefaultRootWindow(self.display), c_str.as_ptr() as *const i8);
-            XSync(self.display, 0 as i32);
+            XStoreName(
+                self.display,
+                XDefaultRootWindow(self.display),
+                c_str.as_ptr() as *const i8,
+            );
+            XSync(self.display, 0_i32);
         }
     }
 }
