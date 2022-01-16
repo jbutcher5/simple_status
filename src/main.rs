64f64980e4d658c998be34959b8ece1a55189c6b
James Butcher <jamesbutcher167@gmail.com>
2022-01-16 15:27:52 +0000

    Update main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 mod status;
+mod modules;
+
+use sysinfo::{System, SystemExt};
 
 fn main() {
-    let x = status::Status::new("test");
+    let mut sys = System::new_all();
+    sys.refresh_all();
+
+    let x = status::Status::new(modules::uptime("Uptime:".to_string(), sys));
     x.set_status();
 }
