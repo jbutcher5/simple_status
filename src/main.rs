4eef14605be491136d63f9232fa0233b605d6796
James Butcher <jamesbutcher167@gmail.com>
2022-01-18 20:38:59 +0000

    Implement new translate system

--- src/main.rs
+++ src/main.rs
@@ -4,28 +4,30 @@ mod status;
 use std::{thread, time};
 use sysinfo::{System, SystemExt};
 
+use crate::modules::StatusModules;
+
 fn main() {
     let mut sys = System::new_all();
 
-    let active_modules = [
-        modules::StatusModules::time,
-        modules::StatusModules::uptime_string,
-        modules::StatusModules::cpu,
-        modules::StatusModules::memory_used,
-    ];
-
-    let prefixes = ["Time -", "Uptime -", "CPU -", "Mem -"];
+    let enabled_modules = ["load", "time", "uptime", "cpu", "mem"];
+    let prefixes = ["Load -", "Time -", "Uptime -", "CPU -", "Mem -"];
     let seperator = "|";
 
     let mut x = status::Status::new(String::new());
 
     loop {
         sys.refresh_all();
-        let data: String = active_modules
+        let data: String = enabled_modules
             .iter()
             .zip(prefixes)
             .fold(String::new(), |acc, x| {
-                format!("{} {} {} {}", acc, seperator, x.1, x.0(&sys))
+                format!(
+                    "{} {} {} {}",
+                    acc,
+                    seperator,
+                    x.1,
+                    sys.translate(x.0).unwrap_or(String::new())
+                )
             });
 
         x.data = data;
