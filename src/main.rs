61accf25234b53666831801e1be66b92d95649e7
James Butcher <jamesbutcher167@gmail.com>
2022-01-23 23:08:44 +0000

    Move the calculation after the elapsed time being greater than the duriation

--- src/main.rs
+++ src/main.rs
@@ -27,30 +27,31 @@ fn main() {
     let mut time_point: Option<Instant> = None;
 
     loop {
-        let module_names: Vec<String> = config.module_names.clone();
-        let module_commands: Vec<String> = config.module_commands.clone();
-
-        sys.dynamic_refresh(&enabled_modules);
-        let data: String = enabled_modules
-            .iter()
-            .zip(&prefixes)
-            .fold(String::new(), |acc, x| {
-                format!(
-                    "{} {} {} {}",
-                    acc,
-                    seperator,
-                    x.1,
-                    sys.translate(
-                        x.0.to_owned(),
-                        module_names.clone(),
-                        module_commands.clone()
-                    )
-                    .unwrap()
-                )
-            })[seperator.len() + 2..]
-            .to_string();
-
         if time_point.is_none() || time_point.unwrap().elapsed().as_millis() >= 500 {
+            let module_names: Vec<String> = config.module_names.clone();
+            let module_commands: Vec<String> = config.module_commands.clone();
+
+            sys.dynamic_refresh(&enabled_modules);
+            let data: String =
+                enabled_modules
+                    .iter()
+                    .zip(&prefixes)
+                    .fold(String::new(), |acc, x| {
+                        format!(
+                            "{} {} {} {}",
+                            acc,
+                            seperator,
+                            x.1,
+                            sys.translate(
+                                x.0.to_owned(),
+                                module_names.clone(),
+                                module_commands.clone()
+                            )
+                            .unwrap()
+                        )
+                    })[seperator.len() + 2..]
+                    .to_string();
+
             status_bar.set_status(data);
             time_point = Some(Instant::now());
         }
