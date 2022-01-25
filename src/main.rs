ddbcea88f47fe1dae320c5b013f5ab2649b14ecb
James Butcher <jamesbutcher167@gmail.com>
2022-01-25 21:40:21 +0000

    Conform to new ModuleData structure interface

--- src/main.rs
+++ src/main.rs
@@ -5,46 +5,37 @@ mod status;
 use home;
 
 use rayon::prelude::*;
-use std::{thread, time::{Duration, Instant}};
-use sysinfo::{System, SystemExt};
+use std::{
+    thread,
+    time::{Duration, Instant},
+};
 
-use crate::{config::StatusConfig, modules::StatusModules};
+use crate::{config::StatusConfig, modules::ModuleData};
 
 fn main() {
-    let mut sys = System::new();
-
     let user_home = home::home_dir()
         .unwrap()
         .join(".config/simple_status/config.toml");
     let config = user_home.get_config();
+
+    let mut module_data = ModuleData::new(config.clone());
 
     let status_bar = status::Status::new();
     let mut time_point: Option<Instant> = None;
 
     loop {
         if time_point.is_none() || time_point.unwrap().elapsed().as_millis() >= 500 {
-            sys.dynamic_refresh(&config.modules);
+            module_data.dynamic_refresh();
 
             let results: Vec<String> = config
                 .modules
                 .par_iter()
-                .map(|x| -> String {
-                    sys.translate(
-                        x.to_string(),
-                        config.module_names.clone(),
-                        config.module_commands.clone(),
-                    )
-                    .unwrap_or_default()
-                })
+                .map(|x| -> String { module_data.translate(x.to_string()).unwrap_or_default() })
                 .collect();
 
-            let data: String = config
-                .prefixes
-                .iter()
-                .zip(&results)
-                .fold(String::new(), |acc, x| {
-                    format!("{} {} {} {}", acc, config.seperator, x.0, x.1)
-                })[config.seperator.len() + 2..]
+            let data: String = results.iter().fold(String::new(), |acc, x| {
+                format!("{} {} {}", acc, &config.seperator, x)
+            })[config.seperator.len() + 2..]
                 .to_string();
 
             status_bar.set_status(data);
