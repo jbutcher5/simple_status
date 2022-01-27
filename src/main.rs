c11c3bcad1c7778d9a3af71dbad7d0ab2f827b26
James Butcher <jamesbutcher167@gmail.com>
2022-01-27 22:02:42 +0000

    Add clap arguments
    This allows for a -c argument to specify the configuration file

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,17 @@ use std::{
     time::{Duration, Instant},
 };
 
+use clap::{App, Arg, ArgMatches};
+
 use crate::modules::ModuleData;
 
 fn main() {
-    let mut module_data = ModuleData::new(".config/simple_status/config.toml");
+    let args = args();
+    let config_path = args
+        .value_of("CONFIG")
+        .unwrap_or(".config/simple_status/config.toml");
+
+    let mut module_data = ModuleData::new(config_path);
 
     let status_bar = status::Status::new();
     let mut time_point: Option<Instant> = None;
@@ -23,4 +30,19 @@ fn main() {
 
         thread::sleep(Duration::from_millis(1));
     }
+}
+
+fn args() -> ArgMatches {
+    return App::new("simple_status")
+        .version(env!("CARGO_PKG_VERSION"))
+        .author("James Butcher <jamesbutcher167@gmail.com>")
+        .about("Easy, Simple, Clean. A dwm modular config bar. Inspired by dwmblocks")
+        .arg(
+            Arg::new("CONFIG")
+                .short('c')
+                .long("config")
+                .takes_value(true)
+                .help("Path to the configuration file"),
+        )
+        .get_matches();
 }
